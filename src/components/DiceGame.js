0ac17a3e3a5cf3946d4fa5752a5b966698257321
Andrew Der <andrew.g.der@gmail.com>
2025-03-19 14:57:12 -0400

    updated logic to only allow players to play once per day

--- src/components/DiceGame.js
+++ src/components/DiceGame.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dice from "./Dice"; // Import the Dice component
 import "../styles/App.css";
 import DiceRollAudio from "./DiceAudio";
@@ -30,6 +30,16 @@ export default function DiceGame() {
   const [playerName, setPlayerName] = useState(""); // New state for player name
   const [gameOver, setGameOver] = useState(false);
   const [lockDisabled, setLockDisabled] = useState(false);
+  const [hasPlayedToday, setHasPlayedToday] = useState(false);
+
+  useEffect(() => {
+    const lastPlayed = localStorage.getItem("lastPlayedDate");
+    const today = new Date().toISOString().split("T")[0];
+
+    if (lastPlayed === today) {
+      setHasPlayedToday(true);
+    }
+  }, []);
 
   const reroll = () => {
     if (lockDisabled) return;
@@ -70,6 +80,7 @@ export default function DiceGame() {
       setScore((prevScore) => prevScore + roundScore);
       setRoundScores((prevScores) => [...prevScores, roundScore]);
       setGameOver(true);
+      playedGame();
     }
     if (currentRound < 5) {
       const roundScore = CalculateScore(rolls[currentRound]);
@@ -122,99 +133,114 @@ export default function DiceGame() {
     setLockDisabled(false);
     setGameOver(false);
   };
+  const playedGame = () => {
+    if (!hasPlayedToday) {
+      const today = new Date().toISOString().split("T")[0];
+      localStorage.setItem("lastPlayedDate", today);
+      setHasPlayedToday(true);
+    }
+  };
 
   return (
     <div className="game-container">
       <h1 className="title">
         Daily Dice Challenge <InfoModal />
       </h1>
-      <h2 className="round">
-        {" "}
-        {gameOver ? "Game Over!" : `Round ${currentRound + 1}/6`}
-      </h2>
-      <p> {gameOver ? "" : `Rerolls left: ${rerollCount}`}</p>
-      <div className="dice-container">
-        {rolls[currentRound]?.map((die, index) => (
-          <Dice
-            key={index}
-            value={die}
-            isRolling={rollingDice.includes(index)}
-            isSelected={selectedDice.includes(index)}
-            onClick={() =>
-              setSelectedDice((prevSelected) =>
-                prevSelected.includes(index)
-                  ? prevSelected.filter((i) => i !== index)
-                  : [...prevSelected, index]
-              )
-            }
-          />
-        ))}
-      </div>
-      <ScorePreview dice={rolls[currentRound]} />
 
-      <div className="button-container">
-        {" "}
-        {!gameOver && (
-          <>
-            {rerollCount > 0 && (
-              <button
-                className="action-button"
-                onClick={reroll}
-                disabled={lockDisabled}
-              >
-                Reroll Unselected
-              </button>
-            )}
-            <button
-              className="action-button"
-              disabled={lockDisabled}
-              onClick={lockInScore}
-            >
-              Lock In Score
-            </button>{" "}
-          </>
-        )}
-        {gameOver && (
-          <div className="game-over">
-            <h3> Enter your name to submit your score!</h3>
-            <input
-              type="text"
-              placeholder="Name"
-              value={playerName}
-              onChange={(e) => setPlayerName(e.target.value)}
-              className="name-input"
-            />
-            <HighScoreSubmit
-              score={score}
-              onScoreSubmitted={() => {
-                resetGame();
-              }}
-              playerName={playerName}
-            />
+      {hasPlayedToday ? (
+        <h2>You have already played today. Come back tomorrow!</h2>
+      ) : (
+        <>
+          <h2 className="round">
+            {" "}
+            {gameOver ? "Game Over!" : `Round ${currentRound + 1}/6`}
+          </h2>
+
+          <p> {gameOver ? "" : `Rerolls left: ${rerollCount}`}</p>
+          <div className="dice-container">
+            {rolls[currentRound]?.map((die, index) => (
+              <Dice
+                key={index}
+                value={die}
+                isRolling={rollingDice.includes(index)}
+                isSelected={selectedDice.includes(index)}
+                onClick={() =>
+                  setSelectedDice((prevSelected) =>
+                    prevSelected.includes(index)
+                      ? prevSelected.filter((i) => i !== index)
+                      : [...prevSelected, index]
+                  )
+                }
+              />
+            ))}
           </div>
-        )}
-      </div>
-      <h2 className="score">Total Score: {score}</h2>
-      <h3 className="round-scores-title">Round Scores</h3>
-      <div className="round-scores-container">
-        {/* Round Numbers */}
-        <div className="round-numbers">
-          {[...Array(6)].map((_, index) => (
-            <div key={index} className="round-number">
-              {index + 1}
-            </div>
-          ))}
-        </div>
+          <ScorePreview dice={rolls[currentRound]} />
+          <div className="button-container">
+            {" "}
+            {!gameOver && (
+              <>
+                {rerollCount > 0 && (
+                  <button
+                    className="action-button"
+                    onClick={reroll}
+                    disabled={lockDisabled}
+                  >
+                    Reroll Unselected
+                  </button>
+                )}
+                <button
+                  className="action-button"
+                  disabled={lockDisabled}
+                  onClick={lockInScore}
+                >
+                  Lock In Score
+                </button>{" "}
+              </>
+            )}
+            {gameOver && (
+              <div className="game-over">
+                <h3> Enter your name to submit your score!</h3>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={playerName}
+                  onChange={(e) => setPlayerName(e.target.value)}
+                  className="name-input"
+                />
+                <HighScoreSubmit
+                  score={score}
+                  onScoreSubmitted={() => {
+                    resetGame();
+                  }}
+                  playerName={playerName}
+                />
+              </div>
+            )}
+          </div>
 
-        {/* Score Boxes */}
-        <div className="round-score-boxes">
-          {[...Array(6)].map((_, index) => (
-            <div key={index} className="round-score-box">
-              {roundScores[index] !== undefined ? roundScores[index] : "-"}
+          <h2 className="score">Total Score: {score}</h2>
+          <h3 className="round-scores-title">Round Scores</h3>
+          <div className="round-scores-container">
+            {/* Round Numbers */}
+            <div className="round-numbers">
+              {[...Array(6)].map((_, index) => (
+                <div key={index} className="round-number">
+                  {index + 1}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+
+            {/* Score Boxes */}
+            <div className="round-score-boxes">
+              {[...Array(6)].map((_, index) => (
+                <div key={index} className="round-score-box">
+                  {roundScores[index] !== undefined ? roundScores[index] : "-"}
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
       <Leaderboard></Leaderboard>
     </div>
   );
